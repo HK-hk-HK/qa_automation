2ba348c2a404c7ac45434f5d1631f311938454f1
Hanna <annah.klymenko@gmail.com>
2022-06-27 17:59:36 +0300

    Adding 4th homework

--- Homework_4/homework4_2_task.py
+++ Homework_4/homework4_2_task.py
@@ -4,7 +4,7 @@
 { "cilpio": 47.999, "a_studio" 42.999, "momo": 49.999,
 "main-service": 37.245, "buy.ua": 38.324,
 "my-store": 37.166, "the_partner": 38.988,
-"sto": 37.720, "rozetka": 38.003}.
+"sto": 37.720, "rozetka": 38.006}.
 Напишіть код, який знайде і виведе на екран назви магазинів,
 ціни яких попадають в діапазон між мінімальною і максимальною ціною. Наприклад:
 lower_limit = 35.9
@@ -18,7 +18,7 @@ upper_limit = 38.988
 shop_dict = {"cilpio": 47.999, "a_studio": 42.999, "momo": 49.999,
              "main-service": 37.245, "buy.ua": 38.324,
              "my-store": 37.166, "the_partner": 38.988,
-             "sto": 37.720, "rozetka": 38.003}
+             "sto": 37.720, "rozetka": 38.006}
 
 matched_keys = []
 
