1dd03f5f92543a2202e2453874c6eba71fb185f8
Hanna <annah.klymenko@gmail.com>
2022-07-03 20:54:20 +0200

    Eddited 5.1 and test 5.1

--- Homework_5/homework_5_1_task.py
+++ Homework_5/homework_5_1_task.py
@@ -3,17 +3,15 @@
 Функція має вивести на екран тип цього аргумента (для визначення типу використайте type)
 """
 
-def print_type(type_argument):
-    return type(type_argument)
+def print_type(argument):
+    print(type(argument))
+    return print(argument)
 
-some_integer = print_type(type_argument=876)
-some_string = print_type(type_argument='test')
-some_float = print_type(type_argument=5.7)
-some_list = print_type(type_argument=[])
 
-print(f'Type of some_integer is: {some_integer}')
-print(f'Type of some_string is: {some_string}')
-print(f'Type of some_float is: {some_float}')
-print(f'Type of some_list is: {some_list}')
+print_type(argument=876)
+print_type(argument='test')
+print_type(argument=5.7)
+print_type(argument=[])
+
 
 
--- Homework_5/tests/test_5_1.py
+++ Homework_5/tests/test_5_1.py
@@ -3,16 +3,22 @@ import pytest
 from Homework_5.homework_5_1_task import print_type
 
 # test positive scenarios
-@pytest.mark.parametrize('value, value_type', [('test', str),
-                                               (5, int),
-                                               (5.5, float),
-                                               ([], list)])
-def test_type_positive(value, value_type):
-    assert type(value) is value_type
-
+def test_print_type_positive(capsys):
+    print_type(type(7))
+    captured = capsys.readouterr()
+    assert 'int' in captured.out
+    print_type(type('test'))
+    captured = capsys.readouterr()
+    assert 'str' in captured.out
+    print_type(type(1.1))
+    captured = capsys.readouterr()
+    assert 'float' in captured.out
+    print_type(type([]))
+    captured = capsys.readouterr()
+    assert 'list' in captured.out
 
 # test negative: if the function without argument
-def test_type_negative():
+def test_print_type_negative(capsys):
     with pytest.raises(TypeError):
         print_type()
 
